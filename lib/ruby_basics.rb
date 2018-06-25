5459f99a820e6d7686efd7181ecb21673ef85706
Ramona M <r1mari@yahoo.com>
2018-06-25 16:08:12 +0000

    automatically backed up by learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,35 @@
-def division(num1, num2)
- 
+def division(num1,num2)
+return num1/num2
 end
 
-def assign_variable(value)
-  
+def assign_variable(name)
+  name="Bob"
 end
 
-def argue
-  
+def argue(greeting)
+  greeting="I'm right and you are wrong!"
+  puts argue
 end
 
-def greeting
-
+def greeting(greeting,name)
+  if greeting +1
+    puts "#{greeting}, there,"
+  else
+    puts "#{greeting}, there, #{name} "
+end
 end
 
 def return_a_value
-
+return_a_value="Nice"
+return_a_value
 end
 
 def last_evaluated_value
+  last_evaluated_value="expert"
 
 end
 
-def pizza_party
+def pizza_party(a="cheese")
+  pizza_party
 
 end
